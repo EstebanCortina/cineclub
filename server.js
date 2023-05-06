807ab6750c4f829b56e1036157fe9c9d4ffa710a
Esteban Cortina <estebannsebastian@gmail.com>
2023-05-05 21:33:25 -0600

    undo

--- server.js
+++ server.js
@@ -16,31 +16,4 @@ const router = require('./routes/index');
 app.use('/', router);
 
 
-//Mongo test
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://esteban:yamJCMbUuRiA88CM@foro.ezbez1p.mongodb.net/?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri, { useUnifiedTopology: true });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    const comments = client.db("forum").collection("comments");
-    const newComment = {
-      author: 'Juan',
-      content: 'Este es un nuevo comentario',
-      date: new Date(),
-    };
-    await comments.insertOne(newComment)
-  } catch (error) {
-    console.error(error.message);
-  } finally {
-    await client.close();
-  }
-}
-run();
 app.listen(3000)
